4c9e073b8233cd710f0dabd506aedff9af387cd7
ananya71singh <75005774+ananya71singh@users.noreply.github.com>
2021-07-20 16:53:38 +0530

    Update build.groovy

--- shared-library-main/vars/build.groovy
+++ shared-library-main/vars/build.groovy
@@ -21,7 +21,7 @@ def call(String repoUrl) {
         }
         stage('package publisher'){
             steps{
-                nexusArtifactUploader credentialsId: 'cc14fffd-8917-4df8-b5dc-53a1207b4819', groupId: 'com.mycompany.app', nexusUrl: 'localhost:8081/', nexusVersion: 'nexus3', protocol: 'http', repository: 'maven-snapshots', version: '1.0-SNAPSHOT'
+               // nexusArtifactUploader credentialsId: 'cc14fffd-8917-4df8-b5dc-53a1207b4819', groupId: 'com.mycompany.app', nexusUrl: 'localhost:8081/', nexusVersion: 'nexus3', protocol: 'http', repository: 'maven-snapshots', version: '1.0-SNAPSHOT'
             }
         }
        }
@@ -29,14 +29,14 @@ def call(String repoUrl) {
 failure {
   script {
     if (currentBuild.currentResult == 'FAILURE') {
-      step([$class: 'Mailer', notifyEveryUnstableBuild: true, recipients: "rachhachandana48@gmail.com", sendToIndividuals: true])
+      step([$class: 'Mailer', notifyEveryUnstableBuild: true, recipients: "ananya.singh1080ti@gmail.com", sendToIndividuals: true])
     }
   }
 }
 success{
     script{
     if (currentBuild.currentResult == 'SUCCESS') {
-      step([$class: 'Mailer', notifyEveryUnstableBuild: true, recipients: "rachhachandana48@gmail.com", sendToIndividuals: true])
+      step([$class: 'Mailer', notifyEveryUnstableBuild: true, recipients: "ananya.singh1080ti@gmail.com", sendToIndividuals: true])
     }
     }
 }
